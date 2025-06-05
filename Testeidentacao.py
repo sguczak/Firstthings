aa901d5e57aa6e9f37bf2820720ce0a4023e48d3
sguczak <sansaoguczak@gmail.com>
2025-06-05 20:24:32 -0300

    Update Testeidentacao.py

--- Testeidentacao.py
+++ Testeidentacao.py
@@ -1,18 +1,20 @@
-#IDENTAÇÃO
+#INDENTAÇÃO
 if 5>2:
     print("Eu vou ali jaja!")
-
+#essa é uma estrutura indentada
 if 5>3:
     print("Aqui não é o lugar correto")
-
+#essa é uma estrutura indentada
 if 10>11:
     print("estou com fome")
-
+#essa é uma estrutura indentada
 if 9>3:
     
     print("Please, close this window, NOW!!")
     print("Just kidding, broda")
 nomee = input ("qual é seu nome?")
 nome = input ("e a sua idade?")
+nome = input ("mais uma informação, qual não é o seu CPF?")
 print("então, seu nome é", nome,) 
 print("e sua idade é", nomee,)
+
