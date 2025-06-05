8c6cc1e2142e332161c788c9ec620a3318165e54
sguczak <sansaoguczak@gmail.com>
2025-06-05 20:21:59 -0300

    secondcommit
    Second versioning

--- Testeidentacao.py
+++ Testeidentacao.py
@@ -1,6 +1,18 @@
 #IDENTAÇÃO
-if 5>5:
+if 5>2:
     print("Eu vou ali jaja!")
 
+if 5>3:
+    print("Aqui não é o lugar correto")
 
-print("Aqui não é o lugar correto")
+if 10>11:
+    print("estou com fome")
+
+if 9>3:
+    
+    print("Please, close this window, NOW!!")
+    print("Just kidding, broda")
+nomee = input ("qual é seu nome?")
+nome = input ("e a sua idade?")
+print("então, seu nome é", nome,) 
+print("e sua idade é", nomee,)
