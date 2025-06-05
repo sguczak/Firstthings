9e77a28907b4b64d57aec22c552d408bcf14fb27
sguczak <sansaoguczak@gmail.com>
2025-06-05 20:00:02 -0300

    myfirstcommit
    I'm versioning

---
+++ Nemesis.py
@@ -0,0 +1,4 @@
+print("Livro:")
+print("Uma Noite de Programação")
+print("Descrição:")
+print("este livro relata a experiência de um aluno com uma aula experimental de T.I.")
---
+++ Testeidentacao.py
@@ -0,0 +1,6 @@
+#IDENTAÇÃO
+if 5>5:
+    print("Eu vou ali jaja!")
+
+
+print("Aqui não é o lugar correto")
